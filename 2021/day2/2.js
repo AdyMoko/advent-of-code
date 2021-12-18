af9c3c7d959e56a9d7fa621ce8bf2d959c513d36
AdyMoko <mocodean_adrian@yahoo.com>
2021-12-18 11:52:52 +0200

    added day 2 solutions, both parts

--- 2021/day2/2.js
+++ 2021/day2/2.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 let horizontalPosition = 0;
 let depth = 0;
+let aim = 0;
 
 function main() {
     fs.readFile('./input.txt', (err, data) =>{
@@ -8,15 +9,15 @@ function main() {
             console.log('something went wrong');    
         } else {        
             const input = data.toString().split('\r\n'); //split the input into an array
-            
             for(let i=0;i<input.length;i++){ //iterate over each item and change the horizontalPosition and depth accordingly
                 const regex = /[0-9]/;
-                if(/forward/.test(input[i])) {                    
+                if(/forward/.test(input[i])) {  
                     horizontalPosition += Number(input[i].match(regex)[0]);
+                    depth += aim * Number(input[i].match(regex)[0]);
                 }else if(/up/.test(input[i])) {
-                    depth -= Number(input[i].match(regex)[0]);
+                    aim -= Number(input[i].match(regex)[0]);
                 }else if(/down/.test(input[i])) {
-                    depth += Number(input[i].match(regex)[0]);
+                    aim += Number(input[i].match(regex)[0]);
                 }               
             }          
             console.log(`the answer is: ${horizontalPosition*depth}`);
